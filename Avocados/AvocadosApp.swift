dd44a2a9028990c3e19c0f1301def13dd0ff988f
Ege Aydemir <egeaydemirr97@gmail.com>
2022-11-04 16:38:47 +0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Ege Aydemir on 4.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
